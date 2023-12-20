19f199041b4c25271e6c11a529f1d78ffe823ab0
rashadma <rashadkhm@code.edu.az>
2023-12-20 15:35:32 +0200

    Added axios and Home page

--- imbd-clone/src/App.jsx
+++ imbd-clone/src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './components/header/Header'
+import Home from './pages/home/Home'
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route index element={<h1>salam</h1>}></Route>
-        <Route path='movie/:id' element={<h1>Movie Detail</h1>}></Route>
-        <Route path='movies/:type' element={<h1>Movies</h1>}></Route>
-        <Route path='/*' element={<h1>Error</h1>}></Route>
-      </Routes>
-    </Router>
+    <div className='App'>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path='movie/:id' element={<h1>Movie Detail</h1>}></Route>
+          <Route path='movies/:type' element={<h1>Movies</h1>}></Route>
+          <Route path='/*' element={<h1>Error</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
