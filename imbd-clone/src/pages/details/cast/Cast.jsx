28387a0c782e3a8cd16397a8e9c4f809a07fd0a6
rashadma <rashadkhm@code.edu.az>
2023-12-25 15:29:26 +0200

    Added skeleton and .env removed from gitignore

--- imbd-clone/src/pages/details/cast/Cast.jsx
+++ imbd-clone/src/pages/details/cast/Cast.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
 import "./style.scss";
-
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import Img from "../../../components/lazyLoadImage/Img";
 import avatar from "../../../assets/avatar.png";
